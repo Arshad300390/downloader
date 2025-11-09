bf36e1f326c7b218260b4238d52c718489450537
arshad300390 <arshad300390@gmail.comm>
2025-11-09 18:40:56 +0500

    frontend work

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import   DownloaderForm  from '@components/DownloaderForm';
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gray-700 text-white p-4">
+      <h1 className="text-3xl font-bold mb-6">🎬 Universal Video Downloader</h1>
+      <DownloaderForm />
+      <footer className="mt-10 text-sm text-gray-500">
+        Built with ❤️ using Next.js + Tailwind
+      </footer>
+    </main>
+  );
 }
